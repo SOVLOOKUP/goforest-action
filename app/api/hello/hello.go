6e9feb541d5e22a97ed0d97b42783bc28ff03ab1
sovlookup <805408477@qq.com>
2020-06-25 20:30:51 +0800

    msg

--- app/api/hello/hello.go
+++ app/api/hello/hello.go
@@ -1,10 +1,45 @@
 package hello
 
 import (
+    "bytes"
+    "encoding/base64"
     "github.com/gogf/gf/net/ghttp"
 )
+
+func init()  {
+
+}
+
+type ImageMsg struct {
+    ID string
+    Image string
+    Msg string
+}
+
+type Image struct {
+    ID string
+    Image *bytes.Buffer
+    Msg string
+}
+
+func (i *ImageMsg)base64ToImage() *Image {
+    ddd, _ := base64.StdEncoding.DecodeString(i.Image) //成图片文件并把文件写入到buffer
+    return &Image{
+        i.ID,
+        bytes.NewBuffer(ddd),
+        i.Msg,
+    }
+}
+
 
 // Hello is a demonstration route handler for output "Hello World!".
 func Hello(r *ghttp.Request) {
     r.Response.Writeln("Hello World!")
+}
+
+func SendImage(r *ghttp.Request) *Image {
+    var imagemsg *ImageMsg
+    _ = r.Parse(&imagemsg)
+    image := imagemsg.base64ToImage()
+    return image
 }
--- go.mod
+++ go.mod
@@ -1,5 +1,8 @@
 module gf-app
 
-require github.com/gogf/gf latest
+require (
+	github.com/gogf/gf v1.13.1
+	github.com/nsqio/go-nsq v1.0.8
+)
 
 go 1.11
